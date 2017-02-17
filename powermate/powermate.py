673cb6cd74a350c581f742392f8d59358eb6b336
janssen <janssen@parc.com>
2017-02-16 19:15:20 -0800

    change to use hidapi instead of pyusb

--- powermate/powermate.py
+++ powermate/powermate.py
@@ -1,88 +1,172 @@
-import usb.core
+import logging
+import sys
+import threading
+import time
 
-PULSE_MODE_DIVIDE = 0
-PULSE_MODE_NORMAL = 1
-PULSE_MODE_MULTIPLY = 2
+# using hidapi Cython interface from https://github.com/trezor/cython-hidapi using BSD license
+import hid
 
-class PowerMate(object):
+class Powermate(object):
+
+    VENDOR_ID = 0x077d
+    PRODUCT_ID = 0x0410
+
+    _REPORT_ID = 0
+    _REPORT_LENGTH = 9
+
+    _SET_STATIC_BRIGHTNESS = 1
+    _SET_PULSE_ASLEEP = 2
+    _SET_PULSE_AWAKE = 3
+    _SET_PULSE_MODE = 4    
+
+    _WHEEL = intern("wheel")
+    _BUTTON = intern("button")
+    _UP = intern("up")
+    _DOWN = intern("down")
+
     def __init__(self):
-        self.dev = usb.core.find(idVendor=0x077d, idProduct=0x0410)
-        if dev == None:
+
+        self.__button_state = None      # unknown
+        self.__callbacks = []
+        self.__dev = hid.device()
+        try:
+            self.__dev.open(self.VENDOR_ID, self.PRODUCT_ID)
+        except:
             raise Exception("Could not find the PowerMate")
 
-        self.dev.set_configuration()
-        
-        if self.dev.is_kernel_driver_active(0):
-            self.dev.detach_kernel_driver(0)
+        logging.debug("Manufacturer: %s, product: %s",
+                      self.__dev.get_manufacturer_string(),
+                      self.__dev.get_product_string())
 
-    def set_brightness(self, brightness):
+        self.__dev.set_nonblocking(1)
+
+    def __command(self, command, *args):
+        featureReport = [self._REPORT_ID, 0x41, 1, command, 0, 0, 0, 0, 0]
+        for i in range(len(args)):
+            featureReport[i + 4] = args[i]
+        self.__dev.send_feature_report(featureReport)
+
+    def __inspect(self):
+        report = self.__dev.get_feature_report(self._REPORT_ID, self._REPORT_LENGTH)
+        return report
+
+    @property
+    def brightness(self):
+        report = self.__inspect()
+        # brightness is third byte
+        return report[3]
+
+    @brightness.setter
+    def brightness(self, brightness):
         """Sets the brightness of the PowerMate's LED
              brightness: A value from 0 (darkest) to 255 (brightest), inclusive
         """
-        dev.ctrl_transfer(0x41, 0x01, 0x01, brightness, [], timeout=0)
+        self.__command(self._SET_STATIC_BRIGHTNESS, brightness)
 
-    def set_pulse(pulse):
+    @property
+    def pulsing(self):
+        report = self.__inspect()
+        return True if (report[4] & 0x01) else False
+
+    @pulsing.setter
+    def pulsing(self, pulse):
         """Sets whether or not to pulse constantly
              pulse: A boolean value indicating whether to pulse (True), or not
              (False)
         """
-        dev.ctrl_transfer(0x41, 0x01, 0x03, int(pulse), [], timeout=99999999)
+        self.__command(self._SET_PULSE_AWAKE, 1 if pulse else 0)
 
-    def set_pulse_asleep(pulse):
+    @property
+    def pulsing_when_asleep(self):
+        report = self.__inspect()
+        return True if (report[4] & 0x04) else False
+
+    @pulsing_when_asleep.setter
+    def pulsing_when_asleep(self, pulse):
         """Sets whether or not to pulse when the computer is asleep
              pulse: A boolean value indicating whether to pulse (True), or not
              (False)
         """
-        dev.ctrl_transfer(0x41, 0x01, 0x02, 0, [], timeout=99999999)
+        self.__command(self._SET_PULSE_ASLEEP, 1 if pulse else 0)
 
-    def set_pulse_speed(speed)
+    @property
+    def pulse_speed(self):
+        report = self.__inspect()
+        speed = report[5]
+        if report[4] & 0x20:        # fast
+            speed += 255
+        elif report[4] & 0x10:      # normal
+            speed = 255
+        else:
+            speed = (254 - speed)
+        return speed
+
+    @pulse_speed.setter
+    def pulse_speed(self, speed):
         """Sets the pulse speed of the PowerMate's LED
              speed: A value from -255 to 255 (inclusive). Lower values are
                slower, higher values are faster
         """
-        if speed < 0:
-            mode = 0
-            speed = -speed
-        elif speed == 0:
-            mode = 1
-        elif speed > 0:
-            mode = 2
-        wValue = (mode << 8) | 0x04 # Compute value: 0x04 is SET_PULSE_MODE command
-        wIndex = (speed << 8) | mode
+        if speed < 255:
+            ptable = 0;
+            speed = 254 - speed
+        elif pulseSpeed == 255:
+            ptable = 1;
+            speed = 0;
+        else:
+            ptable = 2;
+            speed -= 255;
 
-        # Set pulse mode
-        dev.ctrl_transfer(0x41, 0x01, wValue, wIndex, [], timeout=0)
+        self.__command(self._SET_PULSE_MODE, ptable, speed);
+
+    @property
+    def button_state(self):
+        report = self.__inspect()
+        return response[0]
+
+    def register_callback(self, callback):
+        self.__callbacks.append(callback)
+
+    def unregister_callback(self, callback):
+        if callback in self.__callbacks:
+            self.__callbacks.remove(callback)
+
+    def __notify(self, event):
+        for callback in self.__callbacks:
+            callback(event)
+
+    def __parse_event(self, data):
+        button_state = data[0]
+        if button_state != self.__button_state:
+            self.__button_state = button_state
+            self.__notify((self._BUTTON, self._DOWN if button_state else self._UP))
+
+        wheel_delta = data[1]
+        if wheel_delta > 0:
+            if wheel_delta & 0x80:
+                wheel_delta -= 256
+            self.__notify((self._WHEEL, wheel_delta))
+
+    def __watch(self):
+        while True:
+            try:
+                data = self.__dev.read(60, timeout_ms=100)
+                if data:
+                    self.__parse_event(data)
+            except Exception as x:
+                logging.warn("exception '%s' on Powermate watch loop", x)            
+
+    def watch(self):
+        self.__event_thread = threading.Thread(target=self.__watch)
+        self.__event_thread.daemon = True
+        self.__event_thread.start()
 
 
-dev.read(129, 6, timeout=99999999999) # endpoint address, length
+if __name__ == "__main__":
 
-brightness = 255
-
-# Set brightness
-dev.ctrl_transfer(0x41, 0x01, 0x01, brightness, [], timeout=0) # wValue and wIndex are important
-
-# Set pulse awake
-dev.ctrl_transfer(0x41, 0x01, 0x03, 0, [], timeout=99999999) # 0 for off, 1 for on
-
-# Set pulse asleep
-dev.ctrl_transfer(0x41, 0x01, 0x02, 0, [], timeout=99999999) 
-
-pulse_mode = 1 # Can be 0, 1, or 2
-
-wValue = (pulse_mode << 8) | 0x04 # Compute value: 0x04 is SET_PULSE_MODE command
-effect = 255 # Maximum effect, from 0 to 255
-wIndex = (effect << 8) | pulse_mode
-
-# Set pulse mode
-dev.ctrl_transfer(0x41, 0x01, wValue, wIndex, [], timeout=99999999)
-
-""" dev.read return value:
-    Array:
-    0: Button position (0 is not pressed, 1 is pressed)
-    1: Amount turned (positive 1, 2, 3 etc for right, negative 1 (255), 2, 3
-    etc for left
-    2: 0 (meaningless)
-    3: Static brightness (0-255)
-    4: LED status (pulsing or constant brightness, pulse while sleep)
-    5: Pulse value (0-255)
-"""
+    p = Powermate()
+    p.brightness = 100
+    print p.brightness, p.pulse_speed
+    p.register_callback(lambda event: sys.stdout.write(str(event) + "\n"))
+    p.watch()
+    time.sleep(1000)
